b9b0795d056b4b8718614eefb742632045854460
Marcin Stolarek <stolarek.marcin@gmail.com>
2022-02-04 10:01:38 +0100

    Change project name to ansible-deployer

--- tests/02-checkrun.sh
+++ tests/02-checkrun.sh
@@ -28,20 +28,20 @@ check_run_ok() {
 }
 
 # Correct execution.
-check_run_ok "ansible-deploy run -t task_exec_bin_true -s prod -i testInfra"
+check_run_ok "ansible-deployer run -t task_exec_bin_true -s prod -i testInfra"
 
 # Non-existent option values
-check_output_fail 'ansible-deploy run -t task_exec_bin_ERR -s prod -i testInfra' '\[ERROR\]: task_exec_bin_ERR not found in configuration file.'
-check_output_fail 'ansible-deploy run -t task_exec_bin_true -s prod -i testInfra_ERR' '\[ERROR\]: testInfra_ERR not found in configuration file.'
-check_output_fail 'ansible-deploy run -t task_exec_bin_true -s prod_ERR -i testInfra' '\[ERROR\]: prod_ERR not found in configuration file.'
+check_output_fail 'ansible-deployer run -t task_exec_bin_ERR -s prod -i testInfra' '\[ERROR\]: task_exec_bin_ERR not found in configuration file.'
+check_output_fail 'ansible-deployer run -t task_exec_bin_true -s prod -i testInfra_ERR' '\[ERROR\]: testInfra_ERR not found in configuration file.'
+check_output_fail 'ansible-deployer run -t task_exec_bin_true -s prod_ERR -i testInfra' '\[ERROR\]: prod_ERR not found in configuration file.'
 
 # misc
-check_output_fail 'ansible-deploy run -t task_empty -s testing -i testInfra' '\[ERROR\]: No playbooks found for requested task'
+check_output_fail 'ansible-deployer run -t task_empty -s testing -i testInfra' '\[ERROR\]: No playbooks found for requested task'
 
 #Artificially generate lock
-check_run_ok "ansible-deploy lock -s locked -i testInfra"
-check_output_fail 'ansible-deploy run -t task_exec_bin_true -s locked -i testInfra' "is using this infrastructure, please try again later."
+check_run_ok "ansible-deployer lock -s locked -i testInfra"
+check_output_fail 'ansible-deployer run -t task_exec_bin_true -s locked -i testInfra' "is using this infrastructure, please try again later."
 
 #Check --debug option
-check_run_ok "ansible-deploy list --debug" "\[DEBUG\]: load_configuration called"
+check_run_ok "ansible-deployer list --debug" "\[DEBUG\]: load_configuration called"
 
