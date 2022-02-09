bfe79e7845c2cd72b77cafa7e16ffa363c7c2c7d
LegenJCdary <jay_cob@interia.pl>
2022-02-09 15:36:00 +0100

    Initial tag handling added

--- etc/hooks/setup_work_dir.sh
+++ etc/hooks/setup_work_dir.sh
@@ -8,6 +8,19 @@ cat << END > ./runBinTrue.yaml
         shell: "/bin/true"
 END
 
+cat << END > ./runBin.yaml
+- hosts: all
+  connection: "local"
+  tasks:
+      - name: "Run /bin/true"
+        shell: "/bin/true"
+        tags: tag_true
+      - name: "Run /bin/false"
+        shell: "/bin/false"
+        tags: tag_false
+END
+
+
 cat << END > ./test_infra1_inv.yaml
 [testHosts]
 testHost1
