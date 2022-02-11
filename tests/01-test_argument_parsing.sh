5c20e0468db23de6274ee1e041a3455751f14b6f
LegenJCdary <jay_cob@interia.pl>
2022-02-11 04:37:48 -0500

    Change testing functions
    Rename check_output_fail to check_message_in_output
    Add check_message_not_in_output

--- tests/01-test_argument_parsing.sh
+++ tests/01-test_argument_parsing.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -l
 
-check_output_fail() {
-	CMD=$1
-	EXPTEXT=$2
-
-	echo "Check: $CMD"
-
+check_message_in_output() {
+        CMD=$1
+        EXPTEXT=$2
+        echo "Check: $CMD"
         eval "$CMD |& grep '$EXPTEXT'"
-	if [ $? -eq 0 ]
-	then
-		echo "OK: '${CMD}' returned '${EXPTEXT}'"
-	else
-		echo "FAILED: '${CMD}' didn't return '${EXPTEXT}'"
-		exit 1
-	fi
+        if [ $? -eq 0 ]
+        then
+                echo "OK: '${CMD}' returned '${EXPTEXT}'"
+        else
+                echo "FAILED: '${CMD}' didn't return '${EXPTEXT}'"
+                exit 1
+        fi
 }
 
 check_run_ok() {
@@ -30,28 +28,28 @@ check_run_ok() {
 }
 
 #Check wrong combinations
-check_output_fail 'ansible-deployer' 'Too few arguments'
-check_output_fail 'ansible-deployer run' '\[ERROR\]: task is required for run'
-check_output_fail 'ansible-deployer run' '\[ERROR\]: infra is required for run'
-check_output_fail 'ansible-deployer run' '\[ERROR\]: stage is required for run'
-check_output_fail 'ansible-deployer run  -t testTask' '\[ERROR\]: stage is required for run'
-check_output_fail 'ansible-deployer run  -t testTask' '\[ERROR\]: infra is required for run'
-check_output_fail 'ansible-deployer run  -t testTask -i testInfra' 'stage is required for run'
+check_message_in_output 'ansible-deployer' 'Too few arguments'
+check_message_in_output 'ansible-deployer run' '\[ERROR\]: task is required for run'
+check_message_in_output 'ansible-deployer run' '\[ERROR\]: infra is required for run'
+check_message_in_output 'ansible-deployer run' '\[ERROR\]: stage is required for run'
+check_message_in_output 'ansible-deployer run  -t testTask' '\[ERROR\]: stage is required for run'
+check_message_in_output 'ansible-deployer run  -t testTask' '\[ERROR\]: infra is required for run'
+check_message_in_output 'ansible-deployer run  -t testTask -i testInfra' 'stage is required for run'
 
-check_output_fail 'ansible-deployer lock -t testTask -i testInfra' '\[ERROR\]: task is not supported by lock'
-check_output_fail 'ansible-deployer lock -t testTask -i testInfra -s prod' '\[ERROR\]: task is not supported by lock'
-check_output_fail 'ansible-deployer lock -t testTask -s prod' '\[ERROR\]: infra is required for lock'
-check_output_fail 'ansible-deployer lock -t testTask -s prod -c X' '\[ERROR\]: commit is not supported by lock'
-check_output_fail 'ansible-deployer lock -t testTask -l test_hosts_1' '\[ERROR\]: limit is not supported by lock'
+check_message_in_output 'ansible-deployer lock -t testTask -i testInfra' '\[ERROR\]: task is not supported by lock'
+check_message_in_output 'ansible-deployer lock -t testTask -i testInfra -s prod' '\[ERROR\]: task is not supported by lock'
+check_message_in_output 'ansible-deployer lock -t testTask -s prod' '\[ERROR\]: infra is required for lock'
+check_message_in_output 'ansible-deployer lock -t testTask -s prod -c X' '\[ERROR\]: commit is not supported by lock'
+check_message_in_output 'ansible-deployer lock -t testTask -l test_hosts_1' '\[ERROR\]: limit is not supported by lock'
 
-check_output_fail 'ansible-deployer unlock -t testTask -i testInfra' '\[ERROR\]: task is not supported by unlock'
-check_output_fail 'ansible-deployer unlock -t testTask -i testInfra -s prod' '\[ERROR\]: task is not supported by unlock'
-check_output_fail 'ansible-deployer unlock -t testTask -s test' '\[ERROR\]: infra is required for unlock'
-check_output_fail 'ansible-deployer unlock -t testTask -s prod -c X' '\[ERROR\]: commit is not supported by unlock'
-check_output_fail 'ansible-deployer unlock -t testTask -l test_hosts_1' '\[ERROR\]: limit is not supported by unlock'
+check_message_in_output 'ansible-deployer unlock -t testTask -i testInfra' '\[ERROR\]: task is not supported by unlock'
+check_message_in_output 'ansible-deployer unlock -t testTask -i testInfra -s prod' '\[ERROR\]: task is not supported by unlock'
+check_message_in_output 'ansible-deployer unlock -t testTask -s test' '\[ERROR\]: infra is required for unlock'
+check_message_in_output 'ansible-deployer unlock -t testTask -s prod -c X' '\[ERROR\]: commit is not supported by unlock'
+check_message_in_output 'ansible-deployer unlock -t testTask -l test_hosts_1' '\[ERROR\]: limit is not supported by unlock'
 
-check_output_fail 'ansible-deployer list --commit testTask'  '\[ERROR\]: commit is not supported by list'
-check_output_fail 'ansible-deployer list -l test_hosts_1'  '\[ERROR\]: limit is not supported by list'
+check_message_in_output 'ansible-deployer list --commit testTask'  '\[ERROR\]: commit is not supported by list'
+check_message_in_output 'ansible-deployer list -l test_hosts_1'  '\[ERROR\]: limit is not supported by list'
 
 #Check if correct combinations are accepted
 check_run_ok "ansible-deployer run --dry -t task_exec_bin_true -s prod -i testInfra"
@@ -63,4 +61,4 @@ check_run_ok "ansible-deployer list"
 check_run_ok "ansible-deployer list --task=task_exec_bin_true"
 
 #Check if wrong config is rejected
-check_output_fail 'ansible-deployer run --dry -t task_exec_bin_true -i nonExistingInfra -s prod' '\[ERROR\]: nonExistingInfra not found in configuration file'
+check_message_in_output 'ansible-deployer run --dry -t task_exec_bin_true -i nonExistingInfra -s prod' '\[ERROR\]: nonExistingInfra not found in configuration file'
