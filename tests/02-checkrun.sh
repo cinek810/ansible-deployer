2adf28327abfd5c4449caa61b67990ad8d88fbc2
LegenJCdary <jay_cob@interia.pl>
2022-02-18 05:55:47 -0500

    Move testing functions to single file

--- tests/02-checkrun.sh
+++ tests/02-checkrun.sh
@@ -1,45 +1,7 @@
 #!/bin/bash -l
 
-check_message_in_output() {
-        CMD=$1
-        EXPTEXT=$2
-        echo "Check: $CMD"
-        eval "$CMD |& grep '$EXPTEXT'"
-        if [ $? -eq 0 ]
-        then
-                echo "OK: '${CMD}' returned '${EXPTEXT}'"
-        else
-                echo "FAILED: '${CMD}' didn't return '${EXPTEXT}'"
-                exit 1
-        fi
-}
 
-check_message_not_in_output() {
-        CMD=$1
-        EXPTEXT=$2
-        echo "Check: $CMD"
-        eval "$CMD |& grep '$EXPTEXT'"
-        if [ $? -eq 0 ]
-        then
-                echo "FAILED: '${CMD}' returned '${EXPTEXT}'"
-                exit 1
-        else
-                echo "OK: '${CMD}' didn't return '${EXPTEXT}'"
-        fi
-}
-
-check_run_ok() {
-	CMD=$1
-	echo "Check: $CMD"
-	$CMD
-	if [ $? -ne 0 ]
-	then
-		echo "FAILED: ${CMD}"
-		exit 1
-	else
-		echo "OK"
-	fi
-}
+source ./tests/testing_lib.sh
 
 # Correct execution.
 check_run_ok "ansible-deployer run -t task_exec_bin_true -s prod -i testInfra"
