81298be1ed9d440ff2477cc2813855cf4f390890
LegenJCdary <jay_cob@interia.pl>
2022-03-22 11:33:39 -0400

    Fix issue #53
    remove exit messages and raise loglevel to critical

--- tests/02-checkrun.sh
+++ tests/02-checkrun.sh
@@ -13,9 +13,9 @@ check_run_ok "ansible-deployer run -t task_with_limit -s testing -i testInfra2 -
 
 echo -e "   ___ ____                                     _   _\n  / _ \___ \            _____  _____  ___ _   _| |_(_) ___  _ __\n | | | |__) |  _____   / _ \ \/ / _ \/ __| | | | __| |/ _ \| '_ \ \n | |_| / __/  |_____| |  __/>  <  __/ (__| |_| | |_| | (_) | | | |\n  \___/_____|          \___/_/\_\___|\___|\__,_|\__|_|\___/|_| |_|\n \n  _                 _ _     _               _   _\n (_)_ ____   ____ _| (_) __| |   ___  _ __ | |_(_) ___  _ __  ___\n | | '_ \ \ / / _\` | | |/ _\` |  / _ \| '_ \| __| |/ _ \| '_ \/ __|\n | | | | \ V / (_| | | | (_| | | (_) | |_) | |_| | (_) | | | \__ \\n |_|_| |_|\_/ \__,_|_|_|\__,_|  \___/| .__/ \__|_|\___/|_| |_|___/\n                                     |_|\n \n"
 # Non-existent option values
-check_message_in_output 'ansible-deployer run -t task_exec_bin_ERR -s prod -i testInfra' '\[ERROR\]: task_exec_bin_ERR not found in configuration file.'
-check_message_in_output 'ansible-deployer run -t task_exec_bin_true -s prod -i testInfra_ERR' '\[ERROR\]: testInfra_ERR not found in configuration file.'
-check_message_in_output 'ansible-deployer run -t task_exec_bin_true -s prod_ERR -i testInfra' '\[ERROR\]: prod_ERR not found in configuration file.'
+check_message_in_output 'ansible-deployer run -t task_exec_bin_ERR -s prod -i testInfra' '\[CRITICAL\]: task_exec_bin_ERR not found in configuration file.'
+check_message_in_output 'ansible-deployer run -t task_exec_bin_true -s prod -i testInfra_ERR' '\[CRITICAL\]: testInfra_ERR not found in configuration file.'
+check_message_in_output 'ansible-deployer run -t task_exec_bin_true -s prod_ERR -i testInfra' '\[CRITICAL\]: prod_ERR not found in configuration file.'
 
 echo -e "   ___ ____                                     _   _\n  / _ \___ \            _____  _____  ___ _   _| |_(_) ___  _ __\n | | | |__) |  _____   / _ \ \/ / _ \/ __| | | | __| |/ _ \| '_ \ \n | |_| / __/  |_____| |  __/>  <  __/ (__| |_| | |_| | (_) | | | |\n  \___/_____|          \___/_/\_\___|\___|\__,_|\__|_|\___/|_| |_|\n \n      _    _             _\n  ___| | _(_)_ __  _ __ (_)_ __   __ _\n / __| |/ / | '_ \| '_ \| | '_ \ / _\` |\n \__ \   <| | |_) | |_) | | | | | (_| |\n |___/_|\_\_| .__/| .__/|_|_| |_|\__, |\n            |_|   |_|            |___/\n \n"
 # Check infra/stage skipping
@@ -38,9 +38,7 @@ check_message_in_output "ansible-deployer run -t task_with_commit -s testing -i 
 
 echo -e "   ___ ____                                     _   _\n  / _ \___ \            _____  _____  ___ _   _| |_(_) ___  _ __\n | | | |__) |  _____   / _ \ \/ / _ \/ __| | | | __| |/ _ \| '_ \ \n | |_| / __/  |_____| |  __/>  <  __/ (__| |_| | |_| | (_) | | | |\n  \___/_____|          \___/_/\_\___|\___|\__,_|\__|_|\___/|_| |_|\n \n        _   _\n   ___ | |_| |__   ___ _ __ ___\n  / _ \| __| '_ \ / _ \ '__/ __|\n | (_) | |_| | | |  __/ |  \__ \\n  \___/ \__|_| |_|\___|_|  |___/\n \n"
 # misc
-check_message_in_output 'ansible-deployer run -t task_empty -s testing -i testInfra' '\[ERROR\]: No playbooks found for requested task'
-# # unlock infra for further tests
-check_run_ok "ansible-deployer unlock -s testing -i testInfra"
+check_message_in_output 'ansible-deployer run -t task_empty -s testing -i testInfra' '\[CRITICAL\]: No playbooks found for requested task'
 
 #Artificially generate lock
 check_run_ok "ansible-deployer lock -s locked -i testInfra"
@@ -55,8 +53,8 @@ check_message_in_output 'ansible-deployer run -t task_with_limit -s testing -i t
 #Try execution of task without permissions
 if [ $UID -ne 0 ]
 then
-	check_message_in_output "ansible-deployer run -t root_only_task -i testInfra -s testing" "\[ERROR\]: Task forbidden"
+	check_message_in_output "ansible-deployer run -t root_only_task -i testInfra -s testing" "\[CRITICAL\]: Task forbidden"
 else
-	check_message_in_output "ansible-deployer run -t non_root_task -i testInfra -s testing" "\[ERROR\]: Task forbidden"
+	check_message_in_output "ansible-deployer run -t non_root_task -i testInfra -s testing" "\[CRITICAL\]: Task forbidden"
 fi
 
