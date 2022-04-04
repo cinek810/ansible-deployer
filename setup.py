20df4508564a6e48ea2b62f2cbde5a6b75f4d6db
LegenJCdary <jay_cob@interia.pl>
2022-04-04 05:24:15 -0400

    Move static data from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,39 +1,5 @@
 """Setuptools configuration for ansible-deployer"""
-import pathlib
-import os
 from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
-
-
-def read(fname):
-    """Helper function for README"""
-    return open(os.path.join(os.path.dirname(__file__), fname), encoding='UTF-8').read()
-
-README = read(str(HERE)+"/README.md")
-
-# This call to setup() does all the work
-setup(
-    name="ansible-deployer",
-    version="0.0.18",
-    description="Wrapper around ansible-playbook allowing configurable tasks and permissions",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cinek810/ansible-deployer",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3.9"
-    ],
-    packages=["ansible_deployer"],
-    include_package_data=True,
-    install_requires=["pyyaml>=5.3.1", "cerberus>=1.3.1"],
-    entry_points={
-        "console_scripts": [
-            "ansible-deployer = ansible_deployer.command_line:main",
-        ]
-    },
-)
+if __name__ == "__main__":
+    setup()
