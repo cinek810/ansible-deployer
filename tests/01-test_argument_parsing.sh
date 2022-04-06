734ea00bd4c81e74e842d361f701e8a67f4fcc32
LegenJCdary <jay_cob@interia.pl>
2022-04-06 08:44:06 -0400

    Add show subcommand
    Add function show_deployer and format_show_deployer
    Add tests for show subcommand in 01 and 02

--- tests/01-test_argument_parsing.sh
+++ tests/01-test_argument_parsing.sh
@@ -19,7 +19,7 @@ check_message_in_output 'ansible-deployer verify  -t task_exec_bin_true' '\[ERRO
 check_message_in_output 'ansible-deployer run  -t task_exec_bin_true -i testInfra' 'stage is required for run'
 check_message_in_output 'ansible-deployer verify  -t task_exec_bin_true -i testInfra' 'stage is required for verify'
 
-check_message_in_output 'ansible-deployer -t task_exec_bin_true -i testInfra' '\[CRITICAL\]: First positional argument (subcommand) is required! Available commands are: run, list, lock, unlock.'
+check_message_in_output 'ansible-deployer -t task_exec_bin_true -i testInfra' '\[CRITICAL\]: First positional argument (subcommand) is required! Available commands are: run, list, lock, unlock, verify, show.'
 
 check_message_in_output 'ansible-deployer verify  -t task_exec_bin_true -i testInfra -s prod --commit testCommit' 'commit is not supported by verify'
 check_message_in_output 'ansible-deployer lock -t task_exec_bin_true -i testInfra' '\[ERROR\]: task is not supported by lock'
@@ -37,6 +37,15 @@ check_message_in_output 'ansible-deployer unlock -t task_exec_bin_true -l test_h
 check_message_in_output 'ansible-deployer list --commit task_exec_bin_true'  '\[ERROR\]: commit is not supported by list'
 check_message_in_output 'ansible-deployer list -l test_hosts_1'  '\[ERROR\]: limit is not supported by list'
 
+check_message_in_output "ansible-deployer run test" "\[CRITICAL\]: Too many positional arguments! Only subcommand \"show\" can accept following arguments: task, infra."
+check_message_in_output "ansible-deployer verify test" "\[CRITICAL\]: Too many positional arguments! Only subcommand \"show\" can accept following arguments: task, infra."
+check_message_in_output "ansible-deployer lock test" "\[CRITICAL\]: Too many positional arguments! Only subcommand \"show\" can accept following arguments: task, infra."
+check_message_in_output "ansible-deployer unlock test" "\[CRITICAL\]: Too many positional arguments! Only subcommand \"show\" can accept following arguments: task, infra."
+check_message_in_output "ansible-deployer list test" "\[CRITICAL\]: Too many positional arguments! Only subcommand \"show\" can accept following arguments: task, infra."
+check_message_in_output "ansible-deployer show test" "\[CRITICAL\]: Invalid argument test! Subcommand \"show\" can accept only following arguments: task, infra."
+check_message_in_output 'ansible-deployer show --commit task_exec_bin_true'  '\[ERROR\]: commit is not supported by show'
+check_message_in_output 'ansible-deployer show -l test_hosts_1'  '\[ERROR\]: limit is not supported by show'
+
 echo -e "   ___  _                                                      _                          _\n  / _ \/ |           __ _ _ __ __ _ _   _ _ __ ___   ___ _ __ | |_   _ __   __ _ _ __ ___(_)_ __   __ _\n | | | | |  _____   / _\` | '__/ _\` | | | | '_ \` _ \ / _ \ '_ \| __| | '_ \ / _\` | '__/ __| | '_ \ / _\` |\n | |_| | | |_____| | (_| | | | (_| | |_| | | | | | |  __/ | | | |_  | |_) | (_| | |  \__ \ | | | | (_| |\n  \___/|_|          \__,_|_|  \__, |\__,_|_| |_| |_|\___|_| |_|\__| | .__/ \__,_|_|  |___/_|_| |_|\__, |\n                              |___/                                 |_|                           |___/\n                               _                         _     _             _   _\n   ___ ___  _ __ _ __ ___  ___| |_    ___ ___  _ __ ___ | |__ (_)_ __   __ _| |_(_) ___  _ __  ___\n  / __/ _ \| '__| '__/ _ \/ __| __|  / __/ _ \| '_ \` _ \| '_ \| | '_ \ / _\` | __| |/ _ \| '_ \/ __|\n | (_| (_) | |  | | |  __/ (__| |_  | (_| (_) | | | | | | |_) | | | | | (_| | |_| | (_) | | | \__ \\n  \___\___/|_|  |_|  \___|\___|\__|  \___\___/|_| |_| |_|_.__/|_|_| |_|\__,_|\__|_|\___/|_| |_|___/\n \n"
 #Check if correct combinations are accepted
 check_run_ok "ansible-deployer run --dry -t task_exec_bin_true -s prod -i testInfra"
@@ -48,6 +57,8 @@ check_run_ok "ansible-deployer lock --dry -s prod -i testInfra"
 check_run_ok "ansible-deployer unlock --dry -s prod -i testInfra"
 check_run_ok "ansible-deployer list"
 check_run_ok "ansible-deployer list --task=task_exec_bin_true"
+check_run_ok "ansible-deployer show"
+check_run_ok "ansible-deployer show infra"
 
 echo -e "   ___  _                                                      _                          _\n  / _ \/ |           __ _ _ __ __ _ _   _ _ __ ___   ___ _ __ | |_   _ __   __ _ _ __ ___(_)_ __   __ _\n | | | | |  _____   / _\` | '__/ _\` | | | | '_ \` _ \ / _ \ '_ \| __| | '_ \ / _\` | '__/ __| | '_ \ / _\` |\n | |_| | | |_____| | (_| | | | (_| | |_| | | | | | |  __/ | | | |_  | |_) | (_| | |  \__ \ | | | | (_| |\n  \___/|_|          \__,_|_|  \__, |\__,_|_| |_| |_|\___|_| |_|\__| | .__/ \__,_|_|  |___/_|_| |_|\__, |\n                              |___/                                 |_|                           |___/\n                                                    __ _\n __      ___ __ ___  _ __   __ _    ___ ___  _ __  / _(_) __ _\n \ \ /\ / / '__/ _ \| '_ \ / _\` |  / __/ _ \| '_ \| |_| |/ _\` |\n  \ V  V /| | | (_) | | | | (_| | | (_| (_) | | | |  _| | (_| |\n   \_/\_/ |_|  \___/|_| |_|\__, |  \___\___/|_| |_|_| |_|\__, |\n                           |___/                         |___/\n \n"
 #Check if wrong config is rejected
