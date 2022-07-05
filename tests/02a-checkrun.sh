c5bd6f5ae35331f526f76d0cdee18a2cff3edc43
LegenJCdary <jay_cob@interia.pl>
2022-07-05 17:33:40 +0200

    change quotes to " in all strings passed to test bash functions

--- tests/02a-checkrun.sh
+++ tests/02a-checkrun.sh
@@ -14,9 +14,9 @@ check_run_ok "ansible-deployer run --task task_with_limit --stage testing --infr
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n  _                 _ _     _               _   _\n (_)_ ____   ____ _| (_) __| |   ___  _ __ | |_(_) ___  _ __  ___\n | | '_ \ \ / / _\` | | |/ _\` |  / _ \| '_ \| __| |/ _ \| '_ \/ __|\n | | | | \ V / (_| | | | (_| | | (_) | |_) | |_| | (_) | | | \__ \ \n |_|_| |_|\_/ \__,_|_|_|\__,_|  \___/| .__/ \__|_|\___/|_| |_|___/\n                                     |_|\n"
 # Non-existent option values
-check_message_in_output 'ansible-deployer run --task task_exec_bin_ERR --stage prod --infrastructure testInfra' '\[CRITICAL\]: task_exec_bin_ERR not found in configuration file.'
-check_message_in_output 'ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra_ERR' '\[CRITICAL\]: testInfra_ERR not found in configuration file.'
-check_message_in_output 'ansible-deployer run --task task_exec_bin_true --stage prod_ERR --infrastructure testInfra' '\[CRITICAL\]: prod_ERR not found in configuration file.'
+check_message_in_output "ansible-deployer run --task task_exec_bin_ERR --stage prod --infrastructure testInfra" "\[CRITICAL\]: task_exec_bin_ERR not found in configuration file."
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra_ERR" "\[CRITICAL\]: testInfra_ERR not found in configuration file."
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage prod_ERR --infrastructure testInfra" "\[CRITICAL\]: prod_ERR not found in configuration file."
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n      _    _             _\n  ___| | _(_)_ __  _ __ (_)_ __   __ _\n / __| |/ / | '_ \| '_ \| | '_ \ / _\` |\n \__ \   <| | |_) | |_) | | | | | (_| |\n |___/_|\_\_| .__/| .__/|_|_| |_|\__, |\n            |_|   |_|            |___/\n"
 # Check infra/stage skipping
@@ -41,8 +41,8 @@ check_message_in_output "ansible-deployer verify --task task_exec_bin_true --sta
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n        _   _\n   ___ | |_| |__   ___ _ __ ___\n  / _ \| __| '_ \ / _ \ '__/ __|\n | (_) | |_| | | |  __/ |  \__ \ \n  \___/ \__|_| |_|\___|_|  |___/\n"
 # Miscellaneous
-check_message_in_output 'ansible-deployer run --task task_empty --stage testing --infrastructure testInfra' '\[CRITICAL\]: No playitems found for requested task'
-check_message_in_output 'ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra' '\[INFO\]: setup_work_dir finished succesfully'
+check_message_in_output "ansible-deployer run --task task_empty --stage testing --infrastructure testInfra" "\[CRITICAL\]: No playitems found for requested task"
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra" "\[INFO\]: setup_work_dir finished succesfully"
 
 # Check dry run
 check_message_in_output "ansible-deployer run --dry --debug --task task_exec_bin_true --stage prod --infrastructure testInfra" "\[INFO\]: Skipping execution because of --dry-run option"
@@ -50,7 +50,7 @@ check_message_not_in_output "ansible-deployer run --dry --debug --task task_exec
 
 # Artificially generate lock
 check_run_ok "ansible-deployer lock --stage locked --infrastructure testInfra"
-check_message_in_output 'ansible-deployer run --task task_exec_bin_true --stage locked --infrastructure testInfra' "is using this infrastructure, please try again later."
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage locked --infrastructure testInfra" "is using this infrastructure, please try again later."
 check_run_ok "ansible-deployer unlock --stage locked --infrastructure testInfra"
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n              _               _                         _ _  __ _\n   ___  _   _| |_ _ __  _   _| |_   _ __ ___   ___   __| (_)/ _(_) ___ _ __ ___\n  / _ \| | | | __| '_ \| | | | __| | '_ \` _ \ / _ \ / _\` | | |_| |/ _ \ '__/ __|\n | (_) | |_| | |_| |_) | |_| | |_  | | | | | | (_) | (_| | |  _| |  __/ |  \__ \ \n  \___/ \__,_|\__| .__/ \__,_|\__| |_| |_| |_|\___/ \__,_|_|_| |_|\___|_|  |___/\n                 |_|\n"
@@ -58,20 +58,20 @@ echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _  
 check_run_ok "ansible-deployer show --debug" "\[DEBUG\]: load_configuration called"
 
 # Check different output options
-check_message_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra' "\[ERROR\]: TASK \[Run ll\]"
-check_message_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --debug' "\[DEBUG\]: TASK \[Run ll\]"
-check_message_not_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --raw-runner-output' "\[ERROR\]: TASK \[Run ll\]"
-check_message_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --raw-runner-output --debug' "\[DEBUG\]: TASK \[Run ll\]"
+check_message_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra" "\[ERROR\]: TASK \[Run ll\]"
+check_message_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --debug" "\[DEBUG\]: TASK \[Run ll\]"
+check_message_not_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --raw-runner-output" "\[ERROR\]: TASK \[Run ll\]"
+check_message_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra --raw-runner-output --debug" "\[DEBUG\]: TASK \[Run ll\]"
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n  _ _           _ _                 _   _\n | (_)_ __ ___ (_) |_    ___  _ __ | |_(_) ___  _ __\n | | | '_ \` _ \| | __|  / _ \| '_ \| __| |/ _ \| '_ \ \n | | | | | | | | | |_  | (_) | |_) | |_| | (_) | | | |\n |_|_|_| |_| |_|_|\__|  \___/| .__/ \__|_|\___/|_| |_|\n                             |_|\n"
 # Check --limit option
-check_message_in_output 'ansible-deployer run --task task_with_limit --stage testing --infrastructure testInfra2 --limit xyzHost4' 'ERROR\! Specified hosts and/or --limit does not match any hosts'
-check_message_in_output 'ansible-deployer run --task task_without_limit --stage testing --infrastructure testInfra --limit testHost1' '\[CRITICAL\]: Limit testHost1 is not available for task task_without_limit.'
-check_message_in_output 'ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra --limit testHost1' '\[CRITICAL\]: Limit testHost1 is not available for task task_exec_bin_true.'
+check_message_in_output "ansible-deployer run --task task_with_limit --stage testing --infrastructure testInfra2 --limit xyzHost4" "ERROR\! Specified hosts and/or --limit does not match any hosts"
+check_message_in_output "ansible-deployer run --task task_without_limit --stage testing --infrastructure testInfra --limit testHost1" "\[CRITICAL\]: Limit testHost1 is not available for task task_without_limit."
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage prod --infrastructure testInfra --limit testHost1" "\[CRITICAL\]: Limit testHost1 is not available for task task_exec_bin_true."
 
 # Check if deployer exits on 1st play item fail
-check_message_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra' "\[ERROR\]: \"ansible-playbook -v -i ./test_infra1_inv.yaml runll.yaml\" failed due to"
-check_message_not_in_output 'ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra' "\[INFO\]: \"ansible-playbook -v -i ./test_infra1_inv.yaml runBinTrue.yaml\" ran succesfully"
+check_message_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra" "\[ERROR\]: \"ansible-playbook -v -i ./test_infra1_inv.yaml runll.yaml\" failed due to"
+check_message_not_in_output "ansible-deployer run --task task_with_ansible_fail --stage testing --infrastructure testInfra" "\[INFO\]: \"ansible-playbook -v -i ./test_infra1_inv.yaml runBinTrue.yaml\" ran succesfully"
 
 # Check --keep-locked option
 check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage testing --infrastructure testInfra --keep-locked --debug" "\[DEBUG\]: Keep locked infra testInfra:testing ."
@@ -79,12 +79,12 @@ check_run_ok "ansible-deployer unlock --stage testing --infrastructure testInfra
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n      _                               _                                                   _\n  ___| |__   _____      __  ___ _   _| |__   ___ ___  _ __ ___  _ __ ___   __ _ _ __   __| |\n / __| '_ \ / _ \ \ /\ / / / __| | | | '_ \ / __/ _ \| '_ \` _ \| '_ \` _ \ / _\` | '_ \ / _\` |\n \__ \ | | | (_) \ V  V /  \__ \ |_| | |_) | (_| (_) | | | | | | | | | | | (_| | | | | (_| |\n |___/_| |_|\___/ \_/\_/   |___/\__,_|_.__/ \___\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|\n"
 # Check show subcommand
-check_message_in_output 'ansible-deployer show' 'Available infrastructures:'
-check_message_in_output 'ansible-deployer show' 'Available tasks:'
-check_message_in_output 'ansible-deployer show all' 'Available infrastructures:'
-check_message_in_output 'ansible-deployer show all' 'Available tasks:'
-check_message_in_output 'ansible-deployer show infra' 'Available infrastructures:'
-check_message_in_output 'ansible-deployer show task' 'Available tasks:'
+check_message_in_output "ansible-deployer show" "Available infrastructures:"
+check_message_in_output "ansible-deployer show" "Available tasks:"
+check_message_in_output "ansible-deployer show all" "Available infrastructures:"
+check_message_in_output "ansible-deployer show all" "Available tasks:"
+check_message_in_output "ansible-deployer show infra" "Available infrastructures:"
+check_message_in_output "ansible-deployer show task" "Available tasks:"
 
 echo -e "   ___ ____                      _               _\n  / _ \___ \ __ _            ___| |__   ___  ___| | ___ __ _   _ _ __\n | | | |__) / _\` |  _____   / __| '_ \ / _ \/ __| |/ / '__| | | | '_ \ \n | |_| / __/ (_| | |_____| | (__| | | |  __/ (__|   <| |  | |_| | | | |\n  \___/_____\__,_|          \___|_| |_|\___|\___|_|\_\_|   \__,_|_| |_|\n \n                            _         _\n  _ __   ___ _ __ _ __ ___ (_)___ ___(_) ___  _ __  ___\n | '_ \ / _ \ '__| '_ \` _ \| / __/ __| |/ _ \| '_ \/ __|\n | |_) |  __/ |  | | | | | | \__ \__ \ | (_) | | | \__ \ \n | .__/ \___|_|  |_| |_| |_|_|___/___/_|\___/|_| |_|___/\n |_|\n"
 # Check multiple system groups in acl_group
