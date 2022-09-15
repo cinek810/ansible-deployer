bridgecrew-perf6/ansible-deployer-1

Create "short" version of each test script that uses short input option
Extend each option to long option in existing test scripts
Remove equal sign from some options

tests/02a-checkrun_with_self_setup.sh
@@ -4,13 +4,13 @@
 source ./tests/testing_lib.sh
 
 # Check if cwd is correctly switched when using --self-setup option
-check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
-check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
 search_path=$(find_latest_sequence)
 check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
 check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"
-check_message_in_output "ansible-deployer verify --task=task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
-check_message_in_output "ansible-deployer verify --task=task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+check_message_in_output "ansible-deployer verify --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_in_output "ansible-deployer verify --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
 search_path=$(find_latest_sequence)
 check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
 check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"

tests/02a-checkrun_with_self_setup_short.sh
@@ -1,0 +1,16 @@
+#!/bin/bash -l
+
+
+source ./tests/testing_lib.sh
+
+# Check if cwd is correctly switched when using --self-setup option
+check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+search_path=$(find_latest_sequence)
+check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
+check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"
+check_message_in_output "ansible-deployer verify -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_in_output "ansible-deployer verify -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+search_path=$(find_latest_sequence)
+check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
+check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"

cinek810/ansible-deployer

tests/02a-checkrun_with_self_setup.sh
@@ -4,13 +4,13 @@
 source ./tests/testing_lib.sh
 
 # Check if cwd is correctly switched when using --self-setup option
-check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
-check_message_not_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+check_message_in_output "ansible-deployer run --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_not_in_output "ansible-deployer run --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
 search_path=$(find_latest_sequence)
 check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
 check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"
-check_message_in_output "ansible-deployer verify --task=task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
-check_message_not_in_output "ansible-deployer verify --task=task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+check_message_in_output "ansible-deployer verify --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_not_in_output "ansible-deployer verify --task task_exec_bin_true --stage testing --infrastructure testInfra --debug --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
 search_path=$(find_latest_sequence)
 check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
 check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"

tests/02a-checkrun_with_self_setup_short.sh
@@ -1,0 +1,16 @@
+#!/bin/bash -l
+
+
+source ./tests/testing_lib.sh
+
+# Check if cwd is correctly switched when using --self-setup option
+check_message_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_not_in_output "ansible-deployer run -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+search_path=$(find_latest_sequence)
+check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
+check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"
+check_message_in_output "ansible-deployer verify -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[DEBUG\]: Successfully created workdir: /tmp/"
+check_message_not_in_output "ansible-deployer verify -t task_exec_bin_true -s testing -i testInfra -d --self-setup /etc/alt-workdir" "\[INFO\]: Setup completed in /etc/alt-workdir"
+search_path=$(find_latest_sequence)
+check_file_startingwith_in_dir "/etc/alt-workdir" "runBin.yaml"
+check_file_startingwith_in_dir "$search_path" "ansible-deploy_execution_"