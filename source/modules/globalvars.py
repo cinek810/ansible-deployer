19eb8f6c8294493e7c04be8cb3a9836e6224a57a
LegenJCdary <jay_cob@interia.pl>
2023-01-31 06:34:59 +0100

    take full option names for Validators.expand_option_name from globalvars module

--- source/modules/globalvars.py
+++ source/modules/globalvars.py
@@ -5,3 +5,8 @@ CFG_PERMISSIONS = "0o644"
 SUBCOMMANDS = ("run", "lock", "unlock", "verify", "show")
 PRINT_FAIL = '\033[91m'
 PRINT_END = '\033[0m'
+OPTION_EXPANSION = {
+    "infra": "infrastructure",
+    "raw_output": "raw_runner_output",
+    "conf_val": "conf_validation"
+}
