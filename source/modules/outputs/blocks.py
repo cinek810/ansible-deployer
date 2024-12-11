3cc91328b18148b8f7582a524527efdc1ac817bc
LegenJCdary <jay_cob@interia.pl>
2024-12-11 23:09:27 +0100

    Replace spaces with tabs to highlight some values in log_exit_messages
    Join first 2 messages into one line.
    Much needed aesthetical change, stdout is too cluttered.

--- source/modules/outputs/blocks.py
+++ source/modules/outputs/blocks.py
@@ -9,7 +9,6 @@ from os import sep as ossep
 def log_exit_messages(logger: Logger, log_path: str, db_path: str) -> None:
     """Log info about logfile and db at the end of exec"""
     exe_id = log_path.rsplit(ossep, 2)[1]
-    logger.info(f"This execution received ID: {exe_id}")
-    logger.info("More information on this execution can be acquired in:")
-    logger.info(f"logfile: {log_path}")
-    logger.info(f"database: {db_path}")
+    logger.info(f"More information on this execution (ID:    {exe_id}    ) can be acquired in:")
+    logger.info(f"\tlogfile:\t\t\t{log_path}")
+    logger.info(f"\tdatabase:\t\t\t{db_path}")
